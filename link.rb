055271275fdd493469505d1f30cd50446bc6eba2
Kremieniev Igor <Kremieniev Igor>
2016-12-15 23:31:58 +0200

    Notepad V/01 Working

--- link.rb
+++ link.rb
@@ -10,8 +10,17 @@ class Link < Post
   end
 
   def read_from_console
+    puts" Адрес ссылки"
+    @url = STDIN.gets.chomp
+
+    puts"Что за ссылка ?"
+    @text = STDIN.gets.chomp
+
+
   end
 
   def to_strings
+    time_string = "create : #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")}  \n\r \n\r"
+    return [@url, @text, time_string]
   end
 end
